d976d4f669ec359a1ae4d738ead359c5239c6a0d
Mitchel Mayle <mitchel.mayle@gmail.com>
2017-01-23 20:29:28 -0500

    Added first 2 challenges

---
+++ Daily Programmer/Game Of Threes/Game Of Threes.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Of_Threes
+{
+    //Challenge description:
+
+    //The input is a single number: the number at which the game starts. Write a program that plays the Threes game, and outputs a
+    //valid sequence of steps you need to take to get to 1. Each step should be output as the number you start at, followed by
+    // -1 or 1 (if you are adding/subtracting 1 before dividing), or 0 (if you are just dividing). The last line should be 1.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number to begin Game Of Threes: ");
+
+            int input = int.Parse(Console.ReadLine());
+            int modCount = 0;
+            int stepCount = 0;
+
+            Console.WriteLine();
+
+            while (input != 0)
+            {
+                int remainder = input % 3;
+                
+
+                switch (remainder)
+                {
+                    case 0:
+                        Console.WriteLine($"{input}, modified by 0, already divisible by 3");
+                        input /= 3;
+                        stepCount++;
+                        break;
+                    case 1:
+                        Console.WriteLine($"{input}, modified by subtracting 1");
+                        input--;
+                        modCount++;
+                        stepCount++;
+                        break;
+                    case 2:
+                        Console.WriteLine($"{input}, modified by adding 1");
+                        input++;
+                        modCount++;
+                        stepCount++;
+                        break;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"COMPLETE! {modCount} Adjustments were made over the course of {stepCount} total steps.");
+        }
+    }
+}
---
+++ Daily Programmer/Rövarspråket/Rövarspråket.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rövarspråket
+{
+    class Rövarspråket
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word to convert to Rövarspråket: ");
+            string input = Console.ReadLine();
+            string final = "";
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                switch (input[i])
+                {
+                    case 'A':
+                    case 'a':
+                    case 'E':
+                    case 'e':
+                    case 'I':
+                    case 'i':
+                    case 'O':
+                    case 'o':
+                    case 'U':
+                    case 'u':
+                    case 'Y':
+                    case 'y':
+                        final += input[i];
+                        break;
+                    default:
+                        final += input[i] + "o" + input[i];
+                        break;
+                }
+            }
+
+            Console.WriteLine($"Your word \"{input}\" translates to \"{final}\" in Rövarspråket.");
+
+        }
+    }
+}
