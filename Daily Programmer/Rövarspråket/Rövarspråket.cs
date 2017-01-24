79c748e21dd36022a8844c4e8663cf84b2132bf7
Mitchel Mayle <mitchel.mayle@gmail.com>
2017-01-23 20:35:44 -0500

    Added description to Rövarspråket

--- Daily Programmer/Rövarspråket/Rövarspråket.cs
+++ Daily Programmer/Rövarspråket/Rövarspråket.cs
@@ -6,6 +6,13 @@ using System.Threading.Tasks;
 
 namespace Rövarspråket
 {
+    //Challenge description
+
+    // Rövarspråket is not very complicated: you take an ordinary word and replace the consonants with the consonant 
+    // doubled and with an "o" in between. So the consonant "b" is replaced by "bob", "r" is replaced with "ror", "s" is 
+    // replaced with "sos", and so on. Vowels are left intact. It's made for Swedish, but it works just as well in English.
+    // Your task today is to write a program that can encode a string of text into Rövarspråket.
+
     class Rövarspråket
     {
         static void Main(string[] args)
