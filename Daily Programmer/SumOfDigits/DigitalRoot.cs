cd518b3afbc395faadfe3896577df1bb8e531c33
Mitchel Mayle <mitchel.mayle@gmail.com>
2017-02-01 15:58:53 -0500

    Added SumOfDigits

---
+++ Daily Programmer/SumOfDigits/DigitalRoot.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfDigits
+{
+    public class DigitalRoot
+    {
+        public static int AddDigits(long n)
+        {
+            string converted = n.ToString();
+
+            string[] numberString = new string[converted.Length];
+            int[] intString = new int[converted.Length];
+
+            for (int i=0; i < converted.Length; i++)
+            {
+                numberString[i] = converted[i].ToString();
+                intString[i] = int.Parse(numberString[i]);
+            }
+
+            int sum = 0;
+
+            for (int i=0; i<intString.Length; i++)
+            {
+                sum += intString[i];
+            }
+
+            if (sum >= 10)
+            {
+
+            }
+
+            return sum;
+           
+        }
+    }
+}
---
+++ Daily Programmer/SumOfDigits/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SumOfDigits;
+
+namespace SumOfDigits
+{
+    class Program
+    {
+        // adds all digits of given long number and returns sum.
+        // original challenge is to return single digit answer -
+            // if the sum of digits is 23, it should then add 2 + 3 and return the answer as 5
+
+        static void Main(string[] args)
+        {
+            long x = 11111;
+
+            Console.WriteLine($" The sum of the digits of {x} is {DigitalRoot.AddDigits(x)}");
+        }
+    }
+}
