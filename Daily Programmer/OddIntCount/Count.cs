4963f1dbd6c26f6488bdae67f54466be28934555
Mitchel Mayle <mitchel.mayle@gmail.com>
2017-03-29 09:09:59 -0400

    odd int count

---
+++ Daily Programmer/OddIntCount/Count.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddIntCount
+{
+    public class Count
+    {
+        public static int FindOddInt(int[] seq)
+        {
+            Dictionary<int, int> numbers = new Dictionary<int, int>();
+
+            foreach (int number in seq)
+            {
+                if (numbers.ContainsKey(number))
+                {
+                    numbers[number]++;
+                }
+                else
+                {
+                    numbers.Add(number, 1);
+                }
+            }
+
+            foreach (KeyValuePair<int, int> kvp in numbers)
+            {
+                if (kvp.Value % 2 == 1)
+                {
+                    return kvp.Key;
+                }
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ Daily Programmer/OddIntCount/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddIntCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // class returns the integer from an array that has an odd number of occurrences
+            // written with the assumption that only one int in the array will have an odd count
+
+        }
+    }
+}
